dcecfcec0589ace46e0f38be907d050cc982b20a
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-08 20:55:09 +0800

    basic component

---
+++ src/component/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Body;
