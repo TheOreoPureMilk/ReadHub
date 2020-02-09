33965ce8fa01ab4ea5b51161597079289a33c963
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-09 20:21:55 +0800

    路由编写

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './assets/css/App.css';
 import Card from './component/basic/Card';
+import Head from './component/Head'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Head></Head>
       <Card></Card>
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/topics">
 
+          </Route>
+          <Route path="/news">
+
+          </Route>
+          <Route path="/tech">
+
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/component/Head.js
+++ src/component/Head.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
   Link
 } from "react-router-dom";
 
-class Head extends Component {
+class Head extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
@@ -14,7 +13,29 @@ class Head extends Component {
     return (
       <div>
         <Router>
-          
+          <div>
+            <Link to="/topics">
+              <img
+                src="https://cdn.readhub.cn/static/assets/png/readhub_logo_m@2x.78b35cd0.png"
+              >
+              </img>
+            </Link>
+          </div>
+          <div>
+            <Link to="/topics">
+              热门话题
+            </Link>
+          </div>
+          <div>
+            <Link to="/news">
+              科技动态
+            </Link>
+          </div>
+          <div>
+            <Link to="/tech">
+              开发者资讯
+            </Link>
+          </div>
         </Router>
       </div>
     );
