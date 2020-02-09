1d27c1a44fc31d2e7fc58afc89f6d47883a6f81e
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-09 18:21:17 +0800

    开始制作基础卡片组建

--- src/component/basic/Card.js
+++ src/component/basic/Card.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import { Collapse, Icon } from 'antd';
+import React from 'react'
+import 'antd/dist/antd.css'
 
-class Card extends Component {
+const { Panel } = Collapse;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const customPanelStyle = {
+  background: 'gry',
+  borderRadius: 4,
+  marginBottom: 4,
+  border: 0,
+  overflow: 'hidden',
+};
+
+const string = `
+test string
+`
+console.log(string)
+
+class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
   }
   render() {
     return (
-      <div></div>
+      <div>
+        <Collapse
+          bordered={false}
+          defaultActiveKey={['1']}
+          expandIconPosition="right"
+          expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+        >
+          <Panel header={string} key="1" style={customPanelStyle} >
+            <a href="https://www.baidu.com">
+              <p>{text}</p>
+            </a>
+          </Panel>
+          <Panel header="This is panel header 2" key="2" style={customPanelStyle} showArrow={false}>
+            <p>{text}</p>
+          </Panel>
+          <Panel header="This is panel header 3" key="3" style={customPanelStyle} showArrow={false}>
+            <p>{text}</p>
+          </Panel>
+        </Collapse>,
+        mountNode,
+      </div>
     );
   }
 }
