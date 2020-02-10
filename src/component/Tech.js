f31bd4527ff72001dbfc3eaecd91b70dd0adcf29
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-10 20:04:44 +0800

    开始制作开发者资讯组件

---
+++ src/component/Tech.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Card from './basic/Card'
+import axios from 'axios'
+
+class Topic extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    const url = '/technews'
+    const lastCursor = 'lastCursor='
+    const itemNumber = 'pageSize='
+    const that = this
+    axios.get(url + '?' + itemNumber + '6' + '&' + lastCursor)
+      .then((res) => {
+        console.log(res.data.data)
+        that.setState({
+          data: res.data.data
+        })
+      }).catch((err) => { console.log(err) })
+  }
+
+  render() {
+    return (
+      <div>
+        <Card data={this.state.data}></Card>
+      </div>
+    );
+  }
+}
+
+export default Topic;
