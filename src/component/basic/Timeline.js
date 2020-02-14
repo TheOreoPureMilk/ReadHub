2f454d2768d57fc2ae6382299b07aefb7e5dfb2f
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-02-14 19:01:16 +0800

    详情页面链接问题

--- src/component/basic/Timeline.js
+++ src/component/basic/Timeline.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import '../../assets/css/Timeline.css'
-
+import { Link } from "react-router-dom"
 
 function Timeline(props) {
 
+  function reload() {
+    window.location.reload(true)
+  }
   return (
     <div>
       <ul>
         {
           props.more.map((item) => {
             return (
-              <li key={item.id}>
-                <a href={`/${item.id}`} >
-                  <div className="item-time">{item.createdAt.substring(0,10)}</div>
+              <li key={item.id} onClick={reload}>
+                <Link to={`/topics/${item.id}`} >
+                  <div className="item-time">{item.createdAt.substring(0, 10)}</div>
                   <div className="item-title">{item.title}</div>
-                </a>
+                </Link>
               </li>
             )
           })
